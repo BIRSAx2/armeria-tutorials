1fd84ae7658cbc3d10d08124a180aec46830e89b
BIRSAx2 <me@mouhieddine.dev>
2024-09-28 13:01:04 +0200

    Project setup

---
+++ src/main/java/dev/mouhieddine/blog/Main.java
@@ -0,0 +1,24 @@
+package dev.mouhieddine.blog;
+
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        Server server = newServer(8080);
+        server.start().join();
+        logger.info("Server has been started. Serving dummy service at http://127.0.0.1:{}", server.activeLocalPort());
+    }
+
+    static Server newServer(int port) {
+        ServerBuilder sb = Server.builder();
+        return sb.http(port)
+                .service("/", (ctx, req) -> HttpResponse.of("Hello, World!"))
+                .build();
+    }
+}
